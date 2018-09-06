e43a58506be559345763f9f374ec0c24aefd71f4
s_falahati <s_falahati@hotmail.com>
2018-09-06 15:19:36 +0430

    LiteDB updated to the latest version

--- LiteDBViewer/Properties/AssemblyInfo.cs
+++ LiteDBViewer/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Soroush Falahati")]
 [assembly: AssemblyProduct("LiteDB Viewer 4")]
-[assembly: AssemblyCopyright("Copyright © Soroush Falahati 2017")]
+[assembly: AssemblyCopyright("Copyright © Soroush Falahati 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -35,5 +35,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("4.1.0.0")]
-[assembly: AssemblyFileVersion("4.1.0.0")]
+[assembly: AssemblyVersion("4.1.4.0")]
+[assembly: AssemblyFileVersion("4.1.4.0")]
